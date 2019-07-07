9029001676718be8fce2aa150f7d0b5a7dc47752
1170300824 <44288851+1170300824@users.noreply.github.com>
2019-07-07 18:34:00 +0800

    Update travelLiao.java

--- src/P2/turtle/travelLiao.java
+++ src/P2/turtle/travelLiao.java
@@ -2,6 +2,6 @@ package turtle;
 
 public class travelLiao {
 	public void travel() {
-		System.out.print("i make some changes");
+		System.out.print("i make some changes,print a line that show i came");
 	}
 }
